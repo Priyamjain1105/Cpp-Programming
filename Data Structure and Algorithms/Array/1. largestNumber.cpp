996db2b78f8aa2dce81877231a07fc000f8a9e92
[Priyamjain1105] <[Priyamjainofficial@gmail.com]>
2025-07-10 11:05:54 +0530

    changes in Array/largestNumber

--- Data Structure and Algorithms/Array/1. largestNumber.cpp
+++ Data Structure and Algorithms/Array/1. largestNumber.cpp
@@ -1,7 +1,21 @@
+/*
+Need to include vector library first
+Ans use the funtion arr.size() for getting the length of the array
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// brute force: Arrange in decending order
+int large1(vector<int> arr)
+{
+    // sort the array
+    int n = arr.size();
+    cout << arr[n - 1];
+}
+
+// Optimal Solution
 int largestno(vector<int> arr)
 {
     int l1 = arr.size();
